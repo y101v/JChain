919a4aaa3dae13420e112f8f423d2c1e13302345
Rafael Magalhães <rafael.cmagalhaes99@gmail.com>
2024-06-13 10:10:07 +0100

    ..

--- src/main/java/org/example/Block.java
+++ src/main/java/org/example/Block.java
@@ -5,22 +5,39 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 
 public class Block {
+
     public String hash;
-    public String  previousHash;
-    private String data;
-    private long timestamp;
+    public String previousHash;
+    private String data; //our data will be a simple message.
+    private long timeStamp; //as number of milliseconds since 1/1/1970.
+    private int nonce;
 
-
-    public Block(String data, String previousHash) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+    //Block Constructor.
+    public Block(String data,String previousHash ) throws UnsupportedEncodingException, NoSuchAlgorithmException {
         this.data = data;
         this.previousHash = previousHash;
-        this.timestamp = new Date().getTime();
-        this.hash = calculatedHash();
+        this.timeStamp = new Date().getTime();
+
+        this.hash = calculateHash(); //Making sure we do this after we set the other values.
     }
 
-    public String calculatedHash() throws UnsupportedEncodingException, NoSuchAlgorithmException {
-        String calculatedHash = StringUtil.applySha256(previousHash + Long.toString(timestamp) + data);
-        return calculatedHash;
+    //Calculate new hash based on blocks contents
+    public String calculateHash() throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        String calculatedhash = StringUtil.applySha256(
+                previousHash +
+                        Long.toString(timeStamp) +
+                        Integer.toString(nonce) +
+                        data
+        );
+        return calculatedhash;
     }
 
+    public void mineBlock(int difficulty) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
+        while(!hash.substring( 0, difficulty).equals(target)) {
+            nonce ++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,19 +5,54 @@ import com.google.gson.GsonBuilder;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.stream.IntStream;
 
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) throws UnsupportedEncodingException, NoSuchAlgorithmException {,
-        ArrayList<Block> blockchain = new ArrayList<Block>();
+    public static ArrayList<Block> blockchain = new ArrayList<>();
+    public static int difficulty = 6;
 
-        blockchain.add(new Block("Hi im the first block", "0"));
-        blockchain.add(new Block("Yo im the second block",blockchain.get(blockchain.size()-1).hash));
-        blockchain.add(new Block("Hey im the third block",blockchain.get(blockchain.size()-1).hash));
+    public static void main(String[] args) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        // Add blocks to the blockchain
+        blockchain.add(new Block("Hi, I'm the first block", "0"));
+        System.out.println("Trying to Mine block 1...");
+        blockchain.get(0).mineBlock(difficulty);
+
+        blockchain.add(new Block("Yo, I'm the second block", blockchain.get(blockchain.size() - 1).hash));
+        System.out.println("Trying to Mine block 2...");
+        blockchain.get(1).mineBlock(difficulty);
+
+        blockchain.add(new Block("Hey, I'm the third block", blockchain.get(blockchain.size() - 1).hash));
+        System.out.println("Trying to Mine block 3...");
+        blockchain.get(2).mineBlock(difficulty);
+
+        System.out.println("\nBlockchain is Valid: " + isChainValid());
 
         String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println("\nThe block chain: ");
         System.out.println(blockchainJson);
+    }
+
+    public static boolean isChainValid() {
+        return IntStream.range(1, blockchain.size())
+                .parallel()  // Use parallel processing for large blockchains
+                .allMatch(i -> {
+                    Block currentBlock = blockchain.get(i);
+                    Block previousBlock = blockchain.get(i - 1);
+                    // Compare registered hash and calculated hash
+                    try {
+                        if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
+                            System.out.println("Current Hashes not equal");
+                            return false;
+                        }
+                    } catch (UnsupportedEncodingException | NoSuchAlgorithmException e) {
+                        throw new RuntimeException(e);
+                    }
+                    // Compare previous hash and registered previous hash
+                    if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+                        System.out.println("Previous Hashes not equal");
+                        return false;
+                    }
+                    return true;
+                });
     }
 }
